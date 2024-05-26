d89af5eaa60c6c113b7b6222811068ea4c158adf
snssll <1051239026@qq.com>
2024-05-26 15:54:24 +0800

    update: third run

--- main.py
+++ main.py
@@ -22,18 +22,21 @@ with open("user_prompt.txt", "r") as f:
 
 messages_template = [
     {"role": "system", "content": system_prompt},
-    {"role": "user", "content": 'todo'}
+    {"role": "user", "content": "todo"},
 ]
+
 
 def make_user_content(title, task):
     occupation = "Occupation: " + title
     task = "Task: " + task
     return user_prompt_without_tail + "\n" + occupation + "\n" + task
 
+
 def make_messages(title, task):
     messages = messages_template.copy()
     messages[1]["content"] = make_user_content(title, task)
     return messages
+
 
 def get_response(title, task):
     response = requests.post(
@@ -53,29 +56,36 @@ def get_response(title, task):
     data = response.json()
     return data["choices"][0]["message"]["content"]
 
+
 def res_2_file(index, title, task):
     try:
         res = get_response(title, task)
         with open(f"output/{index}.txt", "a") as f:
             f.write(res)
-            # print(index)
     except Exception as e:
         print(f"{index} error: {e}")
+        print("sleep 60s")
+        time.sleep(60)
+        try:
+            res = get_response(title, task)
+            with open(f"output/{index}.txt", "a") as f:
+                f.write(res)
+        except Exception as e:
+            print(f"{index} error: {e} 重试失败")
+
 
 def main():
     df = pd.read_csv("tasks.csv")
-    df['index'] = range(len(df))
+    df["index"] = range(len(df))
 
-    file_list = os.listdir('./output/')
-    file_list = [int(file.split('.')[0]) for file in file_list if file.endswith('.txt')]
+    file_list = os.listdir("./output/")
+    file_list = [int(file.split(".")[0]) for file in file_list if file.endswith(".txt")]
     file_list = set(file_list)
-
 
     for i in tqdm(range(len(df))):
         if i not in file_list:
             res_2_file(df["index"][i], df["Title"][i], df["Task"][i])
-            time.sleep(1)
-    #res_2_file(df["index"][18000], df["Title"][18000], df["Task"][18000])
+
 
 if __name__ == "__main__":
     main()
